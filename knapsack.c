cc71f7b597e756d8589ca092b48492a831362507
AADITH SHERLEY DEVANAD <aadithsd1@gmail.com>
2026-05-13 09:50:07 +0530

    Implement 0/1 Knapsack problem in C

---
+++ knapsack.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <conio.h>
+
+int max(int a, int b) {
+    if(a > b) return a;
+    return b;
+}
+
+int knapsack(int W, int wt[], int val[], int n) {
+    int i, w;
+    int K[20][20];
+
+    for(i = 0; i <= n; i++) {
+        for(w = 0; w <= W; w++) {
+            if(i == 0 || w == 0)
+                K[i][w] = 0;
+            else if(wt[i-1] <= w)
+                K[i][w] = max(val[i-1] + K[i-1][w - wt[i-1]], K[i-1][w]);
+            else
+                K[i][w] = K[i-1][w];
+        }
+    }
+    return K[n][W];
+}
+
+void main() {
+    int n, W, i, res;
+    int val[20], wt[20];
+
+    clrscr();
+
+    printf("Enter number of items: ");
+    scanf("%d", &n);
+
+    printf("Enter values:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &val[i]);
+
+    printf("Enter weights:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &wt[i]);
+
+    printf("Enter capacity: ");
+    scanf("%d", &W);
+
+    res = knapsack(W, wt, val, n);
+
+    printf("Max value = %d", res);
+
+    getch();
+}
